817a712b6eb7da00d3a01f90959849ad3b5a1879
easent <easent93@gmail.com>
2021-04-28 21:15:31 +0500

    strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpants <dpants@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/28 21:12:56 by dpants            #+#    #+#             */
+/*   Updated: 2021/04/28 21:12:56 by dpants           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned	char	*tmp_s1;
+	unsigned	char	*tmp_s2;
+	size_t				i;
+
+	tmp_s1 = (unsigned char *)s1;
+	tmp_s2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (tmp_s1[i] != tmp_s2[i])
+			return (tmp_s1[i] - tmp_s2[i]);
+		i++;
+	}
+	return (0);
+}
