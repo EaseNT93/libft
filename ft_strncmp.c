978b37c43b69aac7a5daf3db37af966115b19a0b
EaseNT93 <77778300+EaseNT93@users.noreply.github.com>
2021-04-28 21:54:22 +0500

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,7 +21,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	tmp_s1 = (unsigned char *)s1;
 	tmp_s2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n)
+	while (i < n && (tmp_s1[i] || tmp_s2[i]))
 	{
 		if (tmp_s1[i] != tmp_s2[i])
 			return (tmp_s1[i] - tmp_s2[i]);
