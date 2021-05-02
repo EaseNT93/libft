e6302c55c5e7f8840d9939b1d591ff3a43ac212d
easent <easent93@gmail.com>
2021-05-02 09:50:42 +0500

    strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpants <dpants@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/02 09:43:22 by dpants            #+#    #+#             */
+/*   Updated: 2021/05/02 09:43:22 by dpants           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+	int		len;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	if (!s1 || !s2)
+		return (NULL);
+	new_str = (char *)malloc(sizeof(char) * len);
+	if (NULL == new_str)
+		return (NULL);
+	while (s1[i])
+		new_str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		new_str[j++] = s2[i++];
+	new_str[j] = '\0';
+	return (new_str);
+}
