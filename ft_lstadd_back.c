cd8abf6beee21281a38fa396102785a9c1d31753
easent <easent93@gmail.com>
2021-05-05 18:51:48 +0500

    lst_bocus

---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpants <dpants@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/05 17:47:41 by dpants            #+#    #+#             */
+/*   Updated: 2021/05/05 17:47:41 by dpants           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*list;
+
+	list = *lst;
+	ft_lstlast(list);
+	list->next = new;
+	new->next = NULL;
+}
---
+++ ft_lstmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpants <dpants@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/05 18:24:39 by dpants            #+#    #+#             */
+/*   Updated: 2021/05/05 18:24:39 by dpants           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*list;
+	t_list	*list_el;
+
+	list_el = NULL;
+	while (lst)
+	{
+		list = ft_lstnew((*f)(lst->content));
+		if (!list)
+		{
+			ft_lstclear(list_el, del);
+			return (NULL);
+		}
+		ft_lstadd_back(list_el, list);
+		lst = lst->next;
+	}
+	return (list_el);
+}
