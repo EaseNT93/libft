af26bf0c171ddf5093f85a2ab5fc0edce3b30ecf
EaseNT93 <easent93@gmail.com>
2021-05-05 19:36:52 +0500

    lst_bonus

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -16,8 +16,12 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
 
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
 	tmp = *lst;
 	ft_lstlast(tmp);
 	tmp->next = new;
-	new->next = NULL;
 }
